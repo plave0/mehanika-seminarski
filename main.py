88889658db11bcd05f15673ee6a1731116be40da
Pavle Ćirić <pavle.ciric@hotmail.com>
2021-05-06 18:43:21 +0200

    Sređivanje koda i dodavanje komentara

--- main.py
+++ main.py
@@ -1,146 +1,143 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Učitavamo podatke iz fajla baseballforce.d
+# U prvoj koloni se nalazi proteklo vreme od početka udarca
+# a u drugoj koloni kojom silom palica deluje na lopticu u tom trenutku
+
+# Podatke u vremenu smeštamo u niz T, a podatke o sili u niz F
+
 data = np.loadtxt("baseballforce.d")
 
 T = data[:,0]
 F = data[:,1]
 
-dt = T[1] - T[0]
+# Masa loptice u gramima (zadata u zadatku)
 m = 0.145
+
+# Vremenski korak je konstantan u nizu T, pa
+# uzimamo razliku između prvo g i drugog
+dt = T[1] - T[0]
+
+# Ugao pod kojim se udara loptica u radijanima (nije zadata u zadaku)
 alfa = np.deg2rad(0)
 
+# Inicijalizujemo nizove gde čuvamo ubrzanje, brzinu i položaj.
+# Za početak iz inicijalizujemo kao prazne nizove dužine 100
+
 lena = 100
+A = np.empty(shape=(lena, 2))
+
 lenv = 100
+V = np.empty(shape=(lenv, 2))
+
 lendt = 100
+DT = np.empty(lendt)
 
-AX = np.zeros(lena)
-AY = np.zeros(lena)
+lenr = 100
+R = np.empty(shape=(lenr, 2))
 
-VX = np.zeros(lenv)
-VY = np.zeros(lenv)
+# Početni položaj
+r = np.array([0, 1.5])
 
-DT = np.zeros(lendt)
+# Početna brzina
+v = np.array([0,0])
 
-RX = np.array([])
-RY = np.array([])
+# Početno ubrzanje
+a = np.array([0, 0])
 
-rx = 0
-ry = 1.5
-
-ax = 0
-ay = 0
-
-vx = 0
-vy = 0
-
+#
 t = 0
 
-a = 0
-
-ia = 0 # brojac za AX i AY
-iv = 0 # brojac za VX i VY
+# Brojači za prolazak kroz nizove
+ia = 0
+iv = 0
+ir = 0
 idt = 0
 
-for f in F:
-
-    a = f/m
-
-    ax = a * np.cos(alfa)
-    ay = a * np.sin(alfa) - 9.81
-
-    AX[ia] = ax
-    AY[ia] = ay
-
-    ia += 1;
-
-    if ia >= lena:
-        lena *= 2
-        AX = np.resize(AX, lena)
-        AY = np.resize(AY, lena)
-
-    vx += dt * ax
-    vy += dt * ay
-
-    VX[iv] = vx
-    VY[iv] = vy
-
-    iv += 1
-
-    if iv >= lenv:
-        lenv *= 2
-        VX = np.resize(VX, lenv)
-        VY = np.resize(VY, lenv)
-
-    rx += vx * dt
-    ry += vy * dt
-
-    DT[idt] = t
-
-    idt += 1
-
-    if idt >= lendt:
-        lendt *= 2
-        DT = np.resize(DT, lendt)
-
-    t += dt
-
-AX = np.resize(AX, ia)
-AY = np.resize(AY, ia)
-
-print("Ubrzanje: " + str(ax) + " " + str (ay) + "\nBrzina: " + str(vx) + " " + str(vy) + "\nIntezitet ubrzanja: " + str(a) + "\nPolozaj: " + str(rx) + " " + str(ry) +
-      "\nVreme: " + str(t))
-
-ax = 0
-ay = -9.81
-
+# Brojač kroz petlju
 brojac = 0
-while ry >= 0:
 
-    vx = vx + dt * ax
-    vy = vy + dt * ay
+# U ovoj petlji prolazimo kroz sve vremenske trenutke iz fajla
+while r[1] >= 0:
 
-    VX[iv] = vx
-    VY[iv] = vy
+    # Ako i dalje deluje sila udarca na lopticu
+    if brojac < len(F):
+         # Računamo intezitet ubrzanja u tom trenutku
+        a = F[brojac]/m
+
+        # Računamo vektorz ubrzanja i čuvamo ga
+        a = np.array([a * np.cos(alfa), a * np.sin(alfa) - 9.81])
+        A[ia] = a
+
+    # U suprotnom, ubrzanje je konstantno
+    else:
+        a = np.array([0, -9.81])
+        # A[ia] = a
+
+    # Uvećavamo brojač i proveravamo da li smo sitgli do kraja niza
+    # Ako jesmo, širimo niz
+
+    # Ojler-Kromerovom metodom računamo brzinu i položaj i čuvamo ih
+    v = v + a * dt
+    r = r + v * dt
+    V[iv] = v
+    R[ir] = r
+
+    # Čuvamo trenutno proteklo vreme i ažuriramo proteklo vreme
+    DT[idt] = t
+    t += dt
+
+    # Uvećavamo brojače i proveravamo da li smo sitgli do kraja niza
+    # Ako jesmo, širimo niz
+
+    if brojac < len(F):
+        ia += 1
+        if ia >= lena:
+            lena *= 2
+            A = np.resize(A, (lena, 2))
 
     iv += 1
-
     if iv >= lenv:
         lenv *= 2
-        VX = np.resize(VX, lenv)
-        VY = np.resize(VY, lenv)
+        V = np.resize(V, (lenv, 2))
 
-    rx = rx + dt * vx
-    ry = ry + dt * vy
-
-    DT[idt] = t
+    ir += 1
+    if ir >= lenr:
+        lenr *= 2
+        R = np.resize(R, (lenr, 2))
 
     idt += 1
-
     if idt >= lendt:
         lendt *= 2
         DT = np.resize(DT, lendt)
 
-    t += dt
+    brojac += 1
 
-VX = np.resize(VX, iv)
-VY = np.resize(VY, iv)
 
+# Skrećujemo nizove tako da se uklope sa njihovim sadržajima
+A = np.resize(A, (ia, 2))
+V = np.resize(V, (iv, 2))
+R = np.resize(R, (ir, 2))
 DT = np.resize(DT, idt)
-
-print("Vreme: " + str(t) + "\nDolet: " + str(rx))
-print(brojac)
 
 plt.figure()
 plt.ylabel("Ubrzanje [m/s^2]")
 plt.xlabel("Vreme [s]")
 plt.title("Ubrzianje u funkciji vremena")
-plt.plot(T, AX)
+plt.plot(T, A)
 plt.show()
 
 plt.figure()
 plt.ylabel("Brzina [m/s]")
 plt.xlabel("Vreme [s]")
 plt.title("Brzina u funkciji vremena")
-plt.plot(DT, VY)
+plt.plot(DT, V)
+plt.show()
+
+plt.figure()
+plt.ylabel("y")
+plt.xlabel("x")
+plt.title("Putanja loptice")
+plt.plot(R[:,0], R[:,1])
 plt.show()
